c3ad11c2dcdeb8f3e5d0d5530e5f0bd745d2e889
Xavi Saavedra Alois <xavi.saavedra.alois@gmail.com>
2024-01-03 17:48:25 +0100

    Newscript
    New script

---
+++ Activate-Features/activate-Features.ps1
@@ -0,0 +1,24 @@
+# Establecer la URL del sitio de SharePoint Online
+$siteUrl = "https://zm0j7.sharepoint.com/sites/TestPnPFeatures"
+
+# Obtener la ruta del archivo CSV que contiene las características de sitio
+$csvPath = "features.csv"
+
+# Leer el archivo CSV
+$features = Import-Csv -Path $csvPath
+
+# Conectar al sitio de SharePoint Online
+Connect-PnPOnline -Url $siteUrl -interactive
+# Recorrer las características de sitio del archivo CSV
+foreach ($feature in $features) {
+    $featureId = $feature.FeatureId
+    $featureName = $feature.FeatureName
+    $featureScope = $feature.Scope
+
+    # Activar la característica de sitio
+    Write-Host "Activando la característica de sitio: $featureName ($featureId)"
+    Enable-PnPFeature -Identity $featureId -Scope $featureScope
+}
+
+# Desconectar del sitio de SharePoint Online
+Disconnect-PnPOnline
